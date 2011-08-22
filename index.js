329ff1b7e278e05428d805abd4ce2fec7b841566
Dmitri Gaskin <dmitrig01@gmail.com>
2011-08-22 10:34:03 -0400

    Add error handling, remove debug code, support deletion

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ module.exports = function(options) {
     Step(function() {
         var self = this;
         // Install sqlite db.
-        db = new sqlite3.Database(options.sqlite, function() {
-            db.run('CREATE TABLE IF NOT EXISTS last_seq (id INTEGER)', function() {
+        db = new sqlite3.Database(options.sqlite, function(err) {
+            if (err) throw err;
+            db.run('CREATE TABLE IF NOT EXISTS last_seq (id INTEGER)', function(err) {
+                if (err) throw err;
                 self();
             });
         });
     }, function() {
         var self = this;
+        options.schema += ', _id INTEGER';
         db.run('CREATE TABLE IF NOT EXISTS ' + options.table + ' (' + options.schema + ')', function (err) {
             if (err) throw err;
             self();
@@ -23,8 +26,8 @@ module.exports = function(options) {
         var self = this;
         db.all('SELECT * FROM last_seq', function(err, data) {
             if (err) throw err;
-            var last_id = ((data && data[0] && data.id) || 0);
-    
+            var last_id = ((data && data[0] && data[0].id) || 0);
+
             request.get({
                 uri: 'http://' +
                     options.couchHost + ':' +
@@ -41,37 +44,35 @@ module.exports = function(options) {
         var group = this.group();
         _(records).each(function(record) {
             var data = options.map(record.doc), next = group();
-            if (!data) {
+            if (!record.deleted && !data) {
                 next();
             }
             else {
-                var stmt = 'INSERT INTO ' + options.table + ' (' + _.map(data, function(v,k) { return "'" + k + "'" }).join(',') + ') VALUES (',
-                    values = [],
-                    args = [];
-                _(data).each(function(value) {
-                    args.push(value);
-                    values.push('?');
-                });
-                stmt += values.join(',') + ')';
-                var del = 'DELETE FROM ' + options.table + ' WHERE ', deleteArgs = [], deleteValues = [];
-                _(options.keys).each(function(key) {
-                   deleteArgs.push(data[key]);
-                   deleteValues.push(key + ' = ?');
-                });
-                del += deleteValues.join(' AND ');
-
-                db.run(del, deleteArgs, function(err) {
+                db.run('DELETE FROM ' + options.table + ' WHERE _id = ?', [ record.id ], function(err) {
                     if (err) throw err;
-                    db.run(stmt, args, function(err) {
-                        if (err) throw err;
+                    if (record.deleted) {
                         next();
-                    });
+                    }
+                    else {
+                        data._id = record.id;
+                        var stmt = 'INSERT INTO ' + options.table + ' (' + _.map(data, function(v,k) { return "'" + k + "'" }).join(',') + ') VALUES (',
+                            values = [],
+                            args = [];
+                        _(data).each(function(value) {
+                            args.push(value);
+                            values.push('?');
+                        });
+                        stmt += values.join(',') + ')';
+                        db.run(stmt, args, function(err) {
+                            if (err) throw err;
+                            next();
+                        });
+                    }
                 });
             }
         });
     }, function() {
         var self = this;
-        console.log('here', last);
         db.run('DELETE FROM last_seq', function(err) {
             if (err) throw err;
             db.run('INSERT INTO last_seq VALUES (?)', [ last ], function(err) {
